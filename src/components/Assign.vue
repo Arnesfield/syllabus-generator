f1c064d4e6836990038d9a2de1c5cd17c134469e
Jefferson Rylee <rylee.jeff385@gmail.com>
2018-02-15 13:35:54 +0800

    Grouped assign types

--- src/components/Assign.vue
+++ src/components/Assign.vue
@@ -1,17 +1,21 @@
 <template>
 <v-container v-bind="{ ['grid-list-' + size]: true }">
-  <v-layout row wrap>
-    <v-flex
-      md4
-      lg3
-      xs12
-      sm6
-      :key="i"
-      v-for="(assign, i) in assigns"
-    >
-      <assign-inst :assign='assign'/>
-    </v-flex>
-  </v-layout>
+  <template v-for="(t, i) in type" v-if="countAssigns(t.n)">
+    <v-subheader :key="'subheader-' + i">{{ t.title }}</v-subheader>
+    <v-layout :key="'layout-' + i" row wrap>
+      <v-flex
+        md4
+        lg3
+        xs12
+        sm6
+        :key="i"
+        v-for="(assign, i) in assigns"
+        v-if="Number(assign.status) == t.n"
+      >
+        <assign-inst :assign='assign'/>
+      </v-flex>
+    </v-layout>
+  </template>
 </v-container>
 </template>
 
@@ -27,7 +31,13 @@ export default {
     url: '/assigns',
     assigns: [],
     loading: false,
-    size: 'lg'
+    size: 'lg',
+    type: [
+      { title: 'Approved', n: 1 },
+      { title: 'Undecided', n: 2 },
+      { title: 'Not yet submitted', n: 3 },
+      { title: 'Denied', n: 0 }
+    ],
   }),
   watch: {
     loading(to, from) {
@@ -41,6 +51,12 @@ export default {
   },
 
   methods: {
+    countAssigns(n) {
+      // check assigns with status n
+      // return the length
+      return this.assigns.filter(e => Number(e.status) == n).length
+    },
+
     fetch() {
       this.loading = true
       this.$http.post(this.url).then((res) => {
@@ -48,12 +64,8 @@ export default {
         if (!res.data.success) {
           throw new Error('Request failure.')
         }
-        // parse all assigns
-        this.assigns = res.data.assigns.map(e => {
-          // e.content = JSON.parse(e.content)
-          return e
-        })
-
+        
+        this.assigns = res.data.assigns
         this.loading = false
       }).catch(e => {
         console.error(e)
